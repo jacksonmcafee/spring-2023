43f1fbb6e6775e75336bbb1b08264c342d44e3a6
Jackson McAfee <jacksonmcafee1@gmail.com>
2023-01-16 19:05:18 -0500

    completed cis4930 mod2 assignment, needs final review

--- CIS4930/mod2/storage_app.py
+++ CIS4930/mod2/storage_app.py
@@ -4,7 +4,7 @@ Date: 2023-01-16
 Assignment: Basic Python App
 Due Date: 2023-01-22
 About this project: A basic menu-driven song storage app. User can add a song (title, album name, artist name, length, rating, review), 
-                    edit previously inputted song information, and return information that they input about that song. In order to satisfy
+                    edit previously input song information, and return information that they input about that song. In order to satisfy
                     the requirement that an integer attribute be range-bound, the maximum song length allowed by the program is 37:30 or 2250 seconds
                     (just enough to accept the song MX by Deftones). Additionally, the review attribute is required and must be over 5 characters long 
                     (again, because I wanted a song such as MX by Deftones to meet the requirements, so song title length was off the table).
@@ -12,7 +12,7 @@ About this project: A basic menu-driven song storage app. User can add a song (t
                     I am a novice at best with Python, so I'm sure that there are a lot of things that I could've done better. I really just wanted
                     this program to work and I might go back and fix it later. We will see!
 
-Assumptions: User is not purposefully trying to break the program in any way, user is able to read and comprehend English, etc.
+Assumptions: User is able to read and comprehend English, etc.
 All work below was performed by Jackson McAfee
 """
 # create storage for item with a struct
@@ -47,7 +47,7 @@ class Song:
         if song_secs == 0:
             song_secs = "00"
 
-        print(f'\nThe song you inputted is... \n------------------------')
+        print(f'\nThe song that you input is... \n------------------------')
         print(f'Song Title: {self.song_title}\nAlbum: {self.song_album}\nArtist: {self.song_artist}\nSong Length: {song_mins}:{song_secs}\nRating: {self.song_rating}')
         print(f'Review: {self.song_review}\n')
 
@@ -117,8 +117,8 @@ def set_artist_name(song):
 def set_review(song):
     while (True):
         review = input(f'Input your review: ')
-        if ((len(review) == review.count(' ')) or len(review) <= 5):
-            print(f'Please input your review again. It cannot be composed of exclusively spaces. It also must be greater than 5 characters.')
+        if ((len(review) == review.count(' ')) or len(review) < 4):
+            print(f'Please input your review again. It cannot be composed of exclusively spaces. It also must be at least 5 characters.')
             continue
         else:
             song.set_review(review)
@@ -126,21 +126,21 @@ def set_review(song):
 
 def set_length(song):
     while (True):
-        length = input(f'Input your song length: ')
+        length = input(f'Input your song length in seconds: ')
                 
         # attempt to cast input to int, catch non-int values and continue
         try:
             length = int(length)
         except ValueError: 
-            print(f'Please input a positive integer.\n')
+            print(f'Please input a positive integer between 1 and 2250.\n')
             continue
         
         # if the song length is positive, accept it
-        if (length > 0):
+        if (length > 0 and length <= 2250):
             song.set_length(length)
             break
         else:
-            print(f'Please input a positive integer.\n')
+            print(f'Please input a positive integer between 1 and 2250.\n')
 
 def set_rating(song):
     while (True):
@@ -151,6 +151,7 @@ def set_rating(song):
             rating = float(rating)
         except ValueError:
             print(f'Please input a real number with no spaces from 0-5.')
+            continue
 
         if (rating >= 0 and rating <= 5):
             song.set_rating(rating)
@@ -173,27 +174,23 @@ def main():
 def menu(song):
     # get user input, loop until user input is an integer value 1-3 
     while(True):
-        str_input = input(f'1. Add/Edit Stored Items\n2. Display Stored Items\n3. Quit Program\n> ')
-        
-        # attempt to cast input to int, catch non-int values and continue
-        try:
-            n = int(str_input)
-        except ValueError: 
-            print(f'Please input an integer from 1-3.\n')
-            continue
+        if song.get_edit() == False:
+            str_input = input(f'A) Add Items\nQ) Quit Program\n> ')
+        else:
+            str_input = input(f'A) Edit Item\nB) Display Item\nQ) Quit Program\n> ')
 
-        # verify if input is valid option (between 1-3), else restart
-        if (n > 0 and n < 4):
+        # verify if input is valid option, else restart
+        if (song.get_edit() == False) and (str_input == 'A' or str_input == 'Q'):
+            break
+        elif (song.get_edit() == True) and (str_input == 'A' or str_input == 'B' or str_input == 'Q'):
             break
         else:
-            print(f'Please input an integer from 1-3.\n')
+            print(f'Please input one of the options as a single character.\n')
 
-    if n == 1:
-        if song.edit == False:
-            add_song(song)
-        else:
-            edit_song(song)
-    elif n == 2:
+    if str_input == 'A':
+        # add song
+        add_song(song)
+    elif str_input == 'B':
         # list stored values 
         list_song(song)
     else:
@@ -201,54 +198,31 @@ def menu(song):
         exit(0)
 
 def add_song(song):
+    tf = song.get_edit()
+    
     # get user input for title
+    if tf : print(f'Current title is: {song.get_title()}')
     set_song_name(song)
 
     # get user input for album name
+    if tf : print(f'Current album name is: {song.get_album()}')
     set_album_name(song)
 
     # get user input for artist name
+    if tf : print(f'Current artist is: {song.get_artist()}')
     set_artist_name(song)
 
     # get user input for song review
+    if tf : print(f'Current review is: {song.get_review()}')
     set_review(song)
     
     # get user input for length in seconds
+    if tf : print(f'Current song length is: {song.get_length()}')
     set_length(song)
 
     # get user input for rating from 0 - 5 (float)
+    if tf : print(f'Current rating is: {song.get_rating()}')
     set_rating(song)
-
-def edit_song(song):
-    while(True):
-        print(f'Input what attribute you want to change... \n')
-        str_input = input(f'1. Song Title\n2. Album Title\n3. Artist Name\n4. Review\n5. Song Length\n6. Song Rating\n> ')
-        
-        # attempt to cast input to int, catch non-int values and continue
-        try:
-            n = int(str_input)
-        except ValueError: 
-            print(f'Please input an integer from 1-6.\n')
-            continue
-
-        # verify if input is valid option (between 1-3), else restart
-        if (n > 0 and n < 7):
-            break
-        else:
-            print(f'Please input an integer from 1-6.\n')
-
-    if n == 1:
-        set_song_name(song)
-    elif n == 2:
-        set_album_name(song)
-    elif n == 3:
-        set_artist_name(song)
-    elif n == 4:
-        set_review(song)
-    elif n == 5:
-        set_length(song)
-    else:
-        set_rating(song)
 
 def list_song(song):
     if (song.get_edit() == False):
