3704de593aae6d6244a76289514c476b59bca402
jackson <73971739+jacksonmcafee@users.noreply.github.com>
2023-03-30 20:47:19 +0000

    project finished, abstracted version of trans.c pushed

--- COP4610/proj3/trans.c
+++ COP4610/proj3/trans.c
@@ -1,200 +1,223 @@
-#define BUF_SIZE 4096
-
-#include <stdio.h>
-#include <stdlib.h>
 #include <fcntl.h>
+#include <unistd.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <errno.h>
 
-#define SHM_NAME "/my_shm"
-#define READ_END 0
-#define WRITE_END 1
-#define BUFFER_SIZE 4096
+#define MEM_SIZE 4096
+#define MEM_NAME "/jmm20g_cop4610"
+#define READING_END 0
+#define WRITING_END 1
 
-const char * shm_buff_name = "/my_shm";
+void handle_child(FILE *dst_file, int parent_to_child[], int child_to_parent[]) {
+    // enter the child process
+    close(parent_to_child[WRITING_END]);
+    close(child_to_parent[READING_END]);
 
-struct{
-    int b_sent;
-    int b_size;
-    int b_return;
-}HEADER;
-
-int main(int argc, char **argv) {
-    if (argc < 3) {
-        printf("\n**Two arguments are required: an input file and an output file. \n");
-        printf("Try again in this format: tans <input-file> <output-file>\n\n");
-        return 1;
+    // create shared memory object, give read/write perms if needed
+    int shared_mem_fd = shm_open(MEM_NAME, O_CREAT | O_RDWR, 0666);
+    if (shared_mem_fd < 0) {
+        perror("Error creating shared memory creation object.");
+        exit(1);
     }
 
-    // get input and output file
-    char *in_file = argv[1];
-    char *out_file = argv[2];
-    FILE *in_ptr;
-    FILE *out_ptr;
-
-    // open input file, if it doesn't exist, create it with "r" flag
-    in_ptr = fopen(in_file, "r");
-
-    // this block doesn't make any sense, why would I be explaining that the output
-    // function doesn't exist when I'm opening the input file? How would I know that?
-    if (in_ptr == NULL) {
-        fprintf(stdout, "\n Output file already exists. Would you like to overwrite it(y|n)?");
-        char choice = fgetc(stdin);
-        if (choice == 'n') return 1;
+    // map shared memory object
+    void *shared_mem = mmap(0, MEM_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shared_mem_fd, 0);
+    if (shared_mem == MAP_FAILED) {
+        perror("Error mapping shared memory.");
+        exit(1);
     }
 
-    // open output file, if it doesn't exist, create it with "w" flag
-    out_ptr = fopen(out_file, "w");
-    if (out_ptr == NULL) {
-        fprintf(stderr, "\nOutput file failed to open. Exiting. \n\n");
-        return 1;
-    }
+    /* In office hours, I saw your example where the loop # was calculated before the actual
+     * process began. When I initially wrote my version, I used that loop/remainder 
+     * setup but I ended up with a huge mess (only somewhat related). When I rewrote it, 
+     * I ended up going with the following setup. 
+     *
+     * block_len is updated by fread() in a while loop in the parent process. Within the while
+     * loop, block_num and block_len are used by the child process where data is written to destination.
+     * The block number is acknowledged by the child process and block_num is incremented. 
+     * Cycle repeats.
+     */
 
+    
+    int current_block_num, current_block_len;
+    while (1) {
+    	// get the block number and length from the parent process
+        read(parent_to_child[READING_END], &current_block_num, sizeof(int));
+        read(parent_to_child[READING_END], &current_block_len, sizeof(int));
 
-    // get file size of input file
-    struct stat st;
-    fstat(in_ptr, &st);
-    int in_size = st.st_size;
-
-    // determine how many times you need to loop through the file
-    int rem = 0;
-    int loops = in_size / BUFFER_SIZE;
-    rem = in_size % BUFFER_SIZE;
-    if (rem != 0) { loops++; }
-
-    int pid;
-    int toChild[2]; // pipe to Child
-    int toParent[2]; // pipe to Parent
-    void *shm_ptr;
-    void *shm_base;
-    int shm_fd;
-
-    // no file descriptors passed with toChild and toParent??
-    // should I not include file descriptors for both before creating pipes between them?
-
-    // create pipe to Child process
-    if (pipe(toChild) == -1) {
-        fprintf(stderr, "\ntoChild pipe creation failed. Exiting. \n\n");
-        return 1;
-    }
-
-    // create pipe to Parent process
-    if (pipe(toParent) == -1) {
-        fprintf(stderr, "\ntoParent pipe creation failed. Exiting. \n\n");
-        return 1;
-    }
-
-    // initialize HEADER
-    HEADER.b_sent = 0;
-    HEADER.b_size = 0;
-    HEADER.b_return = -1;
-	
-	// local variables
-    int blocks_read = 0;
-    int expected_size = 0;
-
-    /* 
-    Following is heavily based on the pipe man page.
-    */
-
-    // spilt into two processes
-    if (pid = fork() < 0) {
-        fprintf(stderr, "\nfork() failed. Exiting.\n\n");
-        return 1;
-    }
-
-    // Parent Process
-    if (pid > 0) {
-        close(toChild[READ_END]);
-        close(toParent[WRITE_END]);
-
-        shm_fd = shm_open(shm_buff_name, O_CREAT | O_RDWR, 0666);
-        if (shm_fd == -1) {
-            fprintf(stderr, "\nParent shared mem creation failed. Exiting. \n\n");
-            return 1;
+	// if block_num == 0, end loop
+        if (current_block_num == 0) {
+            write(child_to_parent[WRITING_END], &current_block_num, sizeof(int));
+            break;
         }
-        ftruncate(shm_fd, BUFFER_SIZE);
-        shm_ptr = mmap(0, BUFFER_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
-        if (shm_ptr == MAP_FAILED) {
-            fprintf(stderr, "\nParent mapping creation failed. Exiting. \n\n");
-            return 1;
-        }
-        shm_base = shm_ptr;
 
-        while (loops > 0) {
-            HEADER.b_sent += 1;
-            HEADER.b_size = BUFFER_SIZE;
-            shm_ptr = shm_base; //Buffer used for failsafe purposes
-            if (loops > 1 || (loops == 1 && remainder == 0)) {
-                expected_size = BUFFER_SIZE;
-                blocks_read = fread(shm_ptr, 1, BUFFER_SIZE, in_ptr);
-            } else {
-                expected_size = remainder;
-                HEADER.b_size = remainder;
-                blocks_read = fread(shm_ptr, 1, remainder, in_ptr);
-            }
-            if (expected_size != blocks_read) {
-                fprintf(stderr, "\nThe expected number of read blocks doesn't match what was actually read\n\n");
-                fprintf(stderr, "Expected: %d\nActual: %d\n\n", expected_size, blocks_read);
-                exit(EXIT_FAILURE);
-            }
-            write(toChild[WRITE_END], &HEADER, sizeof(HEADER)); //notify
-            read(toParent[READ_END], &HEADER, sizeof(HEADER)); //wait
+	// write data from shared memory to destination file
+        fwrite(shared_mem, 1, current_block_len, dst_file);
 
-            if (HEADER.b_sent != HEADER.b_return) {
-                fprintf(stderr, "\nBlock retun does not match block number that was sent\n\n");
-                fprintf(stderr, "Block number sent: %d\nBlock number returned: %d\n\n", HEADER.b_sent, HEADER.b_return);
-                exit(EXIT_FAILURE);
-            }
-        }
-    } else { // End Parent Process
-
-        // Beginning Child Process
-
-        close(toChild[WRITE_END]);
-        close(toParent[READ_END]);
-
-        shm_fd = shm_open(shm_buff_name, O_CREAT | O_RDWR, 0666);
-        if (shm_fd == -1) {
-            fprintf(stderr, "\nChild shared mem creation failed. Exiting. \n\n");
-            return 1;
-        }
-        ftruncate(shm_fd, BUFFER_SIZE);
-        shm_ptr = mmap(0, BUFFER_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
-        if (shm_ptr == MAP_FAILED) {
-            fprintf(stderr, "\nChild mapping creation failed. Exiting. \n\n");
-            return 1;
-        }
-        shm_base = shm_ptr; 
-
-        while (loops > 0) {
-         
-            shm_ptr = shm_base; //Buffer used for failsafe purposes
-            if (loops > 1 || (loops == 1 && remainder == 0)) {
-                expected_size = BUFFER_SIZE;
-                blocks_read = fread(shm_ptr, 1, BUFFER_SIZE, in_ptr);
-            } else {
-                expected_size = remainder;
-                HEADER.b_size = remainder;
-                blocks_read = fread(shm_ptr, 1, remainder, in_ptr);
-            }
-            if (expected_size != blocks_read) {
-                fprintf(stderr, "\nThe expected number of read blocks doesn't match what was actually read\n\n");
-                fprintf(stderr, "Expected: %d\nActual: %d\n\n", expected_size, blocks_read);
-                exit(EXIT_FAILURE);
-            }
-            write(toChild[READ_END], &HEADER, sizeof(HEADER)); //notify
-            read(toParent[WRITE_END], &HEADER, sizeof(HEADER)); //wait
-
-            if (HEADER.b_sent != HEADER.b_return) {
-                fprintf(stderr, "\nBlock retun does not match block number that was sent\n\n");
-                fprintf(stderr, "Block number sent: %d\nBlock number returned: %d\n\n", HEADER.b_sent, HEADER.b_return);
-                exit(EXIT_FAILURE);
-            }
-        }
+	// send acknowledgement back to parent process
+        write(child_to_parent[WRITING_END], &current_block_num, sizeof(int));
     }
+
+    // clean up allocated resources, starting with shared memory
+    munmap(shared_mem, MEM_SIZE);
+    shm_unlink(MEM_NAME);
+
+    // close pipes and destination file
+    close(parent_to_child[READING_END]);
+    close(child_to_parent[WRITING_END]);
+    fclose(dst_file);
 }
-//End Child Process
+
+void handle_parent(FILE *src_file, int parent_to_child[], int child_to_parent[]) {
+    close(parent_to_child[READING_END]);
+    close(child_to_parent[WRITING_END]);
+
+    // create shared memory object, give read/write perms if needed
+    int shared_mem_fd = shm_open(MEM_NAME, O_CREAT | O_RDWR, 0666);
+    if (shared_mem_fd < 0) {
+        perror("Error creating shared memory object.");
+        exit(1);
+    }
+
+    // use ftruncate() to ensure that 4KB blocks are allocated
+    ftruncate(shared_mem_fd, MEM_SIZE);
+
+    // map shared memory object
+    void *shared_mem = mmap(0, MEM_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shared_mem_fd, 0);
+    if (shared_mem == MAP_FAILED) {
+        perror("Error mapping shared memory.");
+        exit(1);
+    }
+
+    int current_block_num = 1;
+    int current_block_len;
+    
+    // read data from source in 4KB chunks
+    while ((current_block_len = fread(shared_mem, 1, MEM_SIZE, src_file)) > 0) {
+     	// send current_block_num and current_block_len to child
+	write(parent_to_child[WRITING_END], &current_block_num, sizeof(int));
+        write(parent_to_child[WRITING_END], &current_block_len, sizeof(int));
+
+        int received_block_num;
+	// receieve acknowledgement from child process
+        read(child_to_parent[READING_END], &received_block_num, sizeof(int));
+
+	// check for block_num inconsistencies
+        if (received_block_num != current_block_num) {
+            fprintf(stderr, "Error: block number mismatch\n");
+            break;
+        }
+
+	// increment current_block_num
+        current_block_num++;
+    }
+
+    // when there are no more chunks to send, send zeroes to child to mark end
+    int zero = 0;
+    write(parent_to_child[WRITING_END], &zero, sizeof(int));
+    write(parent_to_child[WRITING_END], &zero, sizeof(int));
+
+    // receieve acknowledgement from child process
+    int received_block_num;
+    read(child_to_parent[READING_END], &received_block_num, sizeof(int));
+
+    // check for block_num inconsistencies
+    if (received_block_num != 0) {
+        fprintf(stderr, "Error: block number mismatch\n");
+    }
+
+    // clean up resources, starting with shared memory
+    munmap(shared_mem, MEM_SIZE);
+    shm_unlink(MEM_NAME);
+
+    // close pipes and source file
+    close(parent_to_child[WRITING_END]);
+    close(child_to_parent[READING_END]);
+    fclose(src_file);
+}
+
+int main(int argc, char *argv[]) {
+    // check for the right number of arguments
+    if (argc != 3) {
+        fprintf(stderr, "Usage: trans source_file destination_file\n");
+        return 1;
+    }
+
+    // open the source file
+    FILE *src_file = fopen(argv[1], "rb");
+    if (!src_file) {
+	// if the source file fails to open, exit gracefully
+        perror("Failed to open input file");
+        return 1;
+    }
+
+    // check for the destination file
+    FILE *dst_file = fopen(argv[2], "rb");
+    if (dst_file) {
+	// if the destination file already exists.
+        fclose(dst_file);
+        char confirm;
+        printf("Output file already exists. Do you want to overwrite it? (y/n): ");
+        scanf(" %c", &confirm);
+        if (confirm != 'y') {
+            printf("Operation aborted.\n");
+            fclose(src_file);
+            return 1;
+        }
+    }
+
+    // open the destination file
+    dst_file = fopen(argv[2], "wb");
+    if (!dst_file) {
+        perror("Failed to open output file");
+        fclose(src_file);
+        return 1;
+    }
+
+    // create pipes
+    int parent_to_child[2];
+    int child_to_parent[2];
+    
+    // if either pipe creation instance fails, clean up and exit gracefully
+    if (pipe(parent_to_child) < 0) {
+        perror("Error creating parent_to_child.");
+        fclose(src_file);
+        fclose(dst_file);
+        return 1;
+    }
+  
+    if (pipe(child_to_parent) < 0) {
+        perror("Error creating child_to_parent.");
+        close(parent_to_child[READING_END]);
+        close(parent_to_child[WRITING_END]);
+        fclose(src_file);
+        fclose(dst_file);
+        return 1;
+    }    
+
+    pid_t pid = fork();
+
+    /*
+     * This is where I kinda break off from the examples provided by Dr. Uh in our lectures but
+     * shouldn't be too far off. Thank GOD for the shm_open and pipe man pages. 
+     *
+     * The setup for the shm_open and mmap are the same, but the actual writing is 
+     * different. I tried to explain in a few places so I hope it helps. 
+     */
+
+    if (pid < 0) {
+        perror("Forking error");
+    } else if (pid == 0) {
+        handle_child(dst_file, parent_to_child, child_to_parent);
+        exit(0);
+    } else {
+        handle_parent(src_file, parent_to_child, child_to_parent);
+    }
+
+    return 0;
+}
